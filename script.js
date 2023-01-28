dec5d15f21db2339e51c5f2c778c0bd473a2df34
sunny8750 <120244832+sunny8750@users.noreply.github.com>
2023-01-29 05:00:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+function getCount(str) {
+ return str.split(' ').filter(function(num) {
+  return num != ''
+ }).length;
+}
+function countfun(){
+ let wordcount =document.getElementById("evaluatedText").value;
+    
+    let countword = document.getElementById("wordCount").innerHTML=getCount(wordcount);
+}
